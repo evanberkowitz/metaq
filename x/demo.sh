270050873aedd45319d0856cb19967ad2ff0e222
Evan Berkowitz <git@evanberkowitz.com>
2016-03-20 20:42:35 -0700

    Add a basic demo script.

---
+++ x/demo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+###
+### Perform a bunch of setup
+###
+
+function rand {
+    mod=$1
+    if [[ -z "$mod" ]]; then
+        mod=150
+    fi
+    echo "${RANDOM}" | awk -v m=$mod '{print 1+$1%m}'
+}
+
+mkdir todo_demo
+if ! ls todo/* >/dev/null 2>/dev/null; then
+    echo "You have things in your todo folder."
+    echo "Demo is aborting."
+    exit
+    
+fi
+
+for t in {0..7}; do
+    echo "#!/bin/bash
+#METAQ NODES $(rand 5)
+#METAQ GPUS $(rand 8)
+#METAQ MIN_WC_TIME $(rand 150)
+
+echo \"working hard...\"
+sleep $(rand 40)            # The 'hard work' is a sham!
+echo \"done!\"
+
+" > todo/metaq.example.task.$t.sh
+chmod +x todo/metaq.example.task.$t.sh
+done
+
+
+###
+### Do what a run script would do:
+###
+
+METAQ=$(pwd)
+METAQ_JOB_ID=metaq.demo.job.$RANDOM
+METAQ_NODES=16  # It's a lie!
+METAQ_GPUS=32   # It's a lie!
+METAQ_RUN_TIME=300
+
+source x/launch.sh
