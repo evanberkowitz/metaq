c9cca6f13ba18ad510b624d3c9e81303130ad30a
Evan Berkowitz <berkowitz2@llnl.gov>
2018-04-04 02:05:59 -0700

    Ignore task script paths in accounting.
    Addresses issue #14 by only grepping for the resource keywords on the first and second columns,
    which are guaranteed by MQ's resource file format to be of the type
    ±NUMBER RESOURCE
    so that the task script names are ignored and the arithmetic is not corrupted.

--- x/metaq_lib.sh
+++ x/metaq_lib.sh
@@ -105,11 +105,11 @@ function METAQ_FOLDER_CLOCK_REQUIREMENT {
 
 
 function METAQ_AVAILABLE_GPUS {
-    grep -i gpu $METAQ_RESOURCES | awk 'BEGIN{total=0} {total+=$1} END {print total}' 2>/dev/null
+    grep -i nodes <(awk  '{print $1" "$2}' $METAQ_RESOURCES) | awk 'BEGIN{total=0} {total+=$1} END {print total}' 2>/dev/null
 }
 
 function METAQ_AVAILABLE_NODES {
-    grep -i nodes $METAQ_RESOURCES | awk 'BEGIN{total=0} {total+=$1} END {print total}' 2>/dev/null
+    grep -i nodes <(awk  '{print $1" "$2}' $METAQ_RESOURCES) | awk 'BEGIN{total=0} {total+=$1} END {print total}' 2>/dev/null
 }
 
 function METAQ_CURRENT_TASKS {
