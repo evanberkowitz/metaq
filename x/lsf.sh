5138b000b9f9f10bd838d937227d710744a3aa46
Evan Berkowitz <e.berkowitz@fz-juelich.de>
2019-01-24 11:34:20 -0500

    LSF: bjobs rather than lsfjobs

--- x/lsf.sh
+++ x/lsf.sh
@@ -3,5 +3,5 @@
 METAQ_BATCH_SCHEDULER="LSF"
 
 function METAQ_JOB_RUNNING {
-    lsfjobs -r -j $1 2>/dev/null | grep $1 2>/dev/null | grep -v lsfjobs 2>/dev/null 
+    bjobs -u all 2>/dev/null | grep $1 2>/dev/null
 }
